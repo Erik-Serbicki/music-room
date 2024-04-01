b4c80a9b391b09be4152b90bf9e5e820788e15ba
Erik-Serbicki <77897214+Erik-Serbicki@users.noreply.github.com>
2024-04-01 11:43:50 -0700

    Added title to CreateRoom page

--- frontend/src/components/CreateRoom.js
+++ frontend/src/components/CreateRoom.js
@@ -12,5 +12,15 @@ import { Link } from "react-router-dom";
 
 
  export default function CreateRoom(){
-    return <p>This is the create room page.</p>;
+    defaultVotes = 1
+
+    return (
+        <Grid container spacing={1}>
+            <Grid item xs={12} align="center">
+                <Typography component="h4" variant="h4">
+                    Create A Room
+                </Typography>
+            </Grid>
+        </Grid>
+    );
 }
