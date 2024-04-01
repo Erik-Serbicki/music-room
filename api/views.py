d4c13e05e1994a7ac3fdd501aaeaf9102600fd01
Erik-Serbicki <77897214+Erik-Serbicki@users.noreply.github.com>
2024-04-01 11:06:26 -0700

    Updated CreateRoomView

--- api/views.py
+++ api/views.py
@@ -10,7 +10,45 @@ class RoomView(generics.ListAPIView):
     serializer_class = serializers.RoomSerializer
     
 class CreateRoomView(APIView):
+    # Define which serializer we want to use
     serializer_class = serializers.CreateRoomSerializer
     
+    # Send a post request
     def post(self, request, format=None):
-        pass
+        # Does the session exist? If not, let's create one
+        if not self.equest.session.exists(self.request.session.session_key):
+            self.request.session.create()
+        
+        # Send data to the serailzer to be translated into python code
+        serializer = self.serializer_class(data=request.data)
+        # Is the data valid?
+        if serializer.is_valid():
+            # Set the model values to the data from the webpage
+            guest_can_pause = serializer.data.get("guest_can_pause")
+            votes_to_skip = serializer.data.get("votes_to_skip")
+            host = self.request.session.session_key
+            
+            # If the session does already exist, we want to update the existing fields, not create new ones
+            
+            # Returns a list of rooms that satisfy the filter
+            queryset = models.Room.objects.filter(host=host)
+            if queryset.exists():
+                # There will only be one item in the list, but we still need to get just that item
+                room = queryset[0]
+                # Set the values
+                room.guest_can_pause = guest_can_pause
+                room.votes_to_skip = votes_to_skip
+                # Save the room, tell it which fields we are updating
+                room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+                # Return the room in our HTTP response
+                return Response(models.RoomSerializer(room).data, status=status.HTTP_200_OK)
+            else:
+                # Create new room
+                room = models.Room(host=host, guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
+                room.save()
+                # Return the room in our HTTP response
+                return Response(models.RoomSerializer(room).data, status=status.HTTP_201_CREATED)
+            
+        # This should only return if our inputs are bad    
+        return Response({'Error':"Input not valid"}, status=status.HTTP_400_BAD_REQUEST)
+            
