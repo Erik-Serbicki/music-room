202e2f1744279fed074ec41758653f64638c287a
Erik-Serbicki <77897214+Erik-Serbicki@users.noreply.github.com>
2024-05-15 11:13:47 -0700

    added callback redirect to urls

--- spotify/urls.py
+++ spotify/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get-auth-url', views.AuthURL.as_view())
+    path('get-auth-url', views.AuthURL.as_view()),
+    path('redirect', views.spotify_callback),
 ]
